ca41f473f6c6340110b64040df7531891e521a7b
Alex <alex@sudoalex.dev>
2024-07-17 12:21:32 -0600

    Initial Map component

--- app/(pages)/location.tsx
+++ app/(pages)/location.tsx
@@ -2,6 +2,7 @@ import { Paragraph } from "@/components/Paragraph";
 import { Title } from "@/components/Title";
 import GlobalStyles from "@/constants/GlobalStyles";
 import { View } from "react-native";
+import { MapComponent } from "../components/MapComponent";
 
 export default function Location() {
   return (
@@ -13,6 +14,7 @@ export default function Location() {
         accusamus nihil et quis nisi odit modi, facere iusto ipsam deleniti?
       </Paragraph>
       <Paragraph>Visit us at: Street Name, City, Country</Paragraph>
+      <MapComponent />
     </View>
   );
 }
---
+++ app/components/MapComponent.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+import { LocationObject } from "expo-location";
+
+export const MapComponent = () => {
+  const [location, setLocation] = useState<LocationObject | null>();
+  const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
