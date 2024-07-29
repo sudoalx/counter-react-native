71cadd2f2d2f8fec5626413925c71c4bf13688a5
Alex <alex@sudoalex.dev>
2024-07-29 08:48:46 -0600

    Update Map styles

--- app/components/MapComponent.tsx
+++ app/components/MapComponent.tsx
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, TextInput } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  View,
+} from "react-native";
 import * as Location from "expo-location";
 import { LocationObject } from "expo-location";
 import MapView, { Marker } from "react-native-maps";
@@ -8,43 +15,81 @@ export const MapComponent = () => {
   const [location, setLocation] = useState<LocationObject | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
+  const mapRef = useRef<MapView>(null);
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
+  const centerMap = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+
+    if (location?.coords) {
+      // Pre-cached location are stringified which should be a STRING
+      // value and cause app to crash. Parse them to Number(float) value
+      const latitude = location.coords.latitude;
+      const longitude = location.coords.longitude;
+
+      // Gradually animate map to user position
+      const region = {
+        latitudeDelta: 0.0092,
+        longitudeDelta: 0.0092,
+        latitude,
+        longitude,
+      };
+      mapRef.current?.animateToRegion(region, 2000);
+    }
+  };
+
+  useEffect(() => {
+    centerMap();
   }, []);
 
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-  const mapRegion = {
-    latitude: location?.coords.latitude ?? 37.78825,
-    longitude: location?.coords.longitude ?? -122.4324,
-    latitudeDelta: 0.0092,
-    longitudeDelta: 0.0092,
-  };
-  console.log(mapRegion);
-
   return (
-    <View style={styles.container}>
-      <TextInput>
-        <Text style={styles.paragraph}>{text}</Text>
-      </TextInput>
-      <MapView style={styles.map} initialRegion={mapRegion}>
-        <Marker coordinate={mapRegion} title="Current location" />
-      </MapView>
-    </View>
+    <ScrollView style={styles.container}>
+      <TextInput
+        multiline
+        style={{
+          width: "100%",
+          color: "black",
+          padding: 10,
+          marginTop: 40,
+        }}
+        placeholder="Location"
+        value={errorMsg?.length ? errorMsg : JSON.stringify(location)}
+        editable={false}
+      />
+      <View>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 0,
+            longitude: 0,
+            longitudeDelta: 0,
+            latitudeDelta: 0,
+          }}
+          ref={mapRef}
+        >
+          <Marker
+            coordinate={
+              location?.coords
+                ? {
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                  }
+                : { latitude: 0, longitude: 0 }
+            }
+            title="Current location"
+          />
+        </MapView>
+        <TouchableOpacity onPress={centerMap} style={styles.centerButton}>
+          <Text>Center map</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -62,5 +107,13 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  centerButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 5,
   },
 });
