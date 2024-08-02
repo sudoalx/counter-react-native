17f11ae39776f3c5b16fe4ccb9fb3c3d869eebfc
Alex <alex@sudoalex.dev>
2024-08-02 09:33:15 -0600

    feat: Add Gyroscope component for sensor data

--- app/(pages)/sensors.tsx
+++ app/(pages)/sensors.tsx
@@ -2,11 +2,13 @@ import { View } from "react-native";
 import React from "react";
 import { AccelerometerComponent } from "../components/Accelerometer";
 import GlobalStyles from "@/constants/GlobalStyles";
+import { GyroscopeComponent } from "../components/Gyroscope";
 
 const SensorsPage = () => {
   return (
     <View style={GlobalStyles.container}>
       <AccelerometerComponent />
+      <GyroscopeComponent />
     </View>
   );
 };
